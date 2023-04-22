fb0ac21ae743150d933f81b095fd1871af457664
Abel <90abel.avr@gmail.com>
2023-04-22 10:57:46 -0300

    add phrases

--- js/app.js
+++ js/app.js
@@ -1,6 +1,46 @@
 // Variables
 const formulario = document.querySelector("#formulario")
 const listaIdeas = document.querySelector("#lista-ideas")
+const fraseDOM = document.querySelector("#frase")
+const frases = ["Sueña como si fueras a vivir para siempre, vive como si fueses a morir hoy, James Dean",
+"Sé amable, porque toda persona que conoces está librando una gran batalla, Platón", 
+"Sé tú mismo. Todos los demás ya están ocupados, Oscar Wilde",
+"La felicidad no está en lo que tú tienes sino en tu actitud",
+"Si te caíste ayer, levántate hoy, H.G. Wells", 
+"Eres tan increíble como te dejas serlo",
+"Das lo que eres, no lo que recibes",
+"La suerte para triunfar en la vida se llama: “creer en ti” ",
+"Lo único imposible es aquello que no intentas",
+"Caerse está permitido, levantarse es obligatorio",
+"Hecho es mejor que perfecto, Sheryl Sandberg",
+"Para tener éxito la actitud es tan importante como la habilidad, Walter Scott",
+"El secreto para salir adelante es comenzar, Mark Twain",
+"Cuanto más duramente trabajo, más suerte tengo, Gary Player",
+"No has llegado tan lejos solo para llegar hasta aquí",
+"Si tú sabes lo que vales, ve y consigue lo que mereces, Rocky Balboa",
+"Da siempre lo mejor que tienes. Lo que plantes ahora lo cosecharás más tarde, Og Mandino",
+"Si crees que puedes, ya estás a medio camino, Theodore Roosevelt ",
+"Avanza con confianza en la dirección de tus sueños, Thoreau",
+"No sueñes tu vida, vive tu sueño",
+"Debes hacer las cosas que crees que no puedes hacer",
+"Lo mejor está por venir",
+"Sin lluvia no habría arcoiris",
+"Los obstáculos son esas cosas atemorizantes que ves cuando apartas los ojos de tu meta, Henry Ford",
+"El conocimiento es poder, Francis Bacon",
+"Dale a cada día la posibilidad de ser el mejor día de tu vida",
+"Va a pasar, porque tú vas a hacer que pase",
+"Da el primer paso con fe. No tienes que ver todas las escaleras, solo da el primer paso, Martin Luther King",
+"No importa lo lento que vayas, siempre y cuando no te detengas",
+"Si te caes tres veces, levántate cuatro",
+"Sí puedes!, Siempre puedes!",
+"La única diferencia entre un buen y un mal día es tu actitud",
+"No cuentes los días, haz que los días cuenten, Muhammad Ali",
+"El mejor momento del día es ahora, Pierre Bonnard",
+"Sé el cambio que quieres ver en el mundo, Mahatma Gandhi",
+"El sentido de la vida es tener valores, no cosas de valor",
+"Cree que puedes y casi lo habrás logrado, Theodore Roosevelt",
+"La mejor forma de predecir el futuro es creándolo, Peter Druker"
+]
 let ideas = []
 // Even Listeners
 eventListeners()
@@ -11,6 +51,9 @@ function eventListeners(){
   // Cuando el documento esta listo 
   document.addEventListener('DOMContentLoaded', ()=>{
     ideas = JSON.parse(localStorage.getItem('ideas')) || []
+    const indice = Math.floor(Math.random() * frases.length);
+    const fraseAleatoria = frases[indice];
+    fraseDOM.innerHTML = fraseAleatoria;
     console.log(ideas);
     crearHTML()
   } )
