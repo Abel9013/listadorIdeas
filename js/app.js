79179704e3f54b2d52afa8fe39bdf185494eed25
Abel <90abel.avr@gmail.com>
2023-04-22 10:04:46 -0300

    add delete function

--- js/app.js
+++ js/app.js
@@ -5,7 +5,15 @@ let ideas = []
 // Even Listeners
 eventListeners()
 function eventListeners(){
+  // Cuando el usuario agrega una nueva idea 
   formulario.addEventListener('submit',agregarIdea)
+
+  // Cuando el documento esta listo 
+  document.addEventListener('DOMContentLoaded', ()=>{
+    ideas = JSON.parse(localStorage.getItem('ideas')) || []
+    console.log(ideas);
+    crearHTML()
+  } )
 
 }
 
@@ -39,19 +47,51 @@ function crearHTML(){
   limpiarHTML()
   if(ideas.length > 0) {
     ideas.forEach( ideaIt => {
+      // Boton de eliminar
+      const btnDelete = document.createElement('a')
+      btnDelete.classList.add('borrar-tweet')
+      btnDelete.innerText = 'X'
+      //Añadir funcion eliminar , uso esta sintaxis para poder pasar un parametro
+      btnDelete.onclick = ()=>{
+        borrarIdea(ideaIt.id)
+      } 
+
       // Creando HTML
       const li = document.createElement("li")
       // Añadir texto
       li.innerText = ideaIt.idea
+      // Asignando el boton 
+      li.appendChild(btnDelete)
+
       listaIdeas.appendChild(li)
     });
   }
+
+  sincronizarStorage()
+
+}
+// Agregar al Storage 
+function sincronizarStorage(){
+  localStorage.setItem('ideas', JSON.stringify(ideas))
 }
 
 function limpiarHTML(){
   while(listaIdeas.firstChild){
     listaIdeas.removeChild(listaIdeas.firstChild)
   }
+}
+// Eliminar Idea 
+function borrarIdea (id){
+ console.log("Borrando id:", id); 
+ const ideasFiltradas = ideas.filter((ideaIterada)=> ideaIterada.id != id
+  // if( ideaIterada.id != id){
+  // Misma sintaxis pero puedo resumirla omitiendo las llaves del arrow
+  //   return ideaIterada
+  // }
+  
+  )
+ ideas = ideasFiltradas
+ crearHTML()
 }
 // Mostrar mensaje de error 
 function mostrarError(error){
